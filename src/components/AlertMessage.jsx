9907cc66c6715f79814bff99a138c6cb8b7e511e
VishalPowerCash <vishal.powercash@gmail.com>
2024-10-17 16:00:37 +0530

    Delete src directory

--- src/components/AlertMessage.jsx
+++
@@ -1,8 +0,0 @@
-export default function AlertMessage({ className, buttonText, text, variant }) {
-    return (
-        <div className={`alert alert-success text-center ${className}`} role="alert">
-
-            {text}{buttonText && <button className={`btn btn-sm ${variant} ml-3`}>{buttonText}</button>}
-        </div>
-    )
-}
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./home/Home";
-import UserAgreement from "./UserAgreement";
-import Privacy from "./Privacy";
-import Loc from "./products/loc/Loc";
-
-export { Home, UserAgreement, Privacy, Loc };
